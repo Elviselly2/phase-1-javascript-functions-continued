cc46f6c3bc018597d4ce1a6a5986ad04f2df6f43
elvis <elvisotieno24@gmail.com>
2025-03-04 22:58:59 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+// wrapAdjective function
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+// Example calls:
+console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("party")); // "This Saturday, I want to party!"
+
+console.log(mondayWork()); // "This Monday, I will go to the office."
+console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
